dcaf4b3cd6fc8f3ed8f79933a6a5b6dfda9da8e2
Omer Yair <omeryair@gmail.com>
2020-10-23 00:44:37 +0300

    Moodle data: open in new tab

--- src/pages/moodle.js
+++ src/pages/moodle.js
@@ -2,14 +2,32 @@ import React from "react"
 import SEO from "../components/seo"
 // import { graphql } from "gatsby"
 
+class MoodlePage extends React.Component {
+  componentDidMount() {
+    var links = document.getElementsByTagName('a');
+    var len = links.length;
+
+    for(var i=0; i<len; i++)
+    {
+      links[i].target = "_blank";
+    }
+  }
+
+  render() {
+    console.log('Render lifecycle')
+    return (
+      <div className="site-content moodle-data">
+        <SEO title="Moodle data" />
+        <article dangerouslySetInnerHTML={{ __html: this.props.data.data_moodle.html }}/>
+        <article dangerouslySetInnerHTML={{ __html: this.props.data.data_all.html }}/>
+      </div>
+    )
+  }
+
+}
+
 const Home = ({ data }) => {
-  return (
-    <div className="site-content moodle-data">
-      <SEO title="Moodle data" />
-      <article dangerouslySetInnerHTML={{ __html: data.data_moodle.html }}/>
-      <article dangerouslySetInnerHTML={{ __html: data.data_all.html }}/>
-    </div>
-  )
+  return (<MoodlePage data={data}/>)
 }
 
 export const query = graphql`
