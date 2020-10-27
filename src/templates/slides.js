fd3bfd305e5ba214f2fc575f274324eb73b02c81
Omer Yair <omeryair@gmail.com>
2020-10-27 03:20:28 +0200

    Bug fix - running slides on netlify

--- src/templates/slides.js
+++ src/templates/slides.js
@@ -1,47 +1,47 @@
-import React from "react"
-import Reveal from "reveal.js"
+import React, { useEffect } from "react"
 import SEO from "../components/seo"
-import { graphql } from "gatsby"
+import { withPrefix, graphql } from "gatsby"
 import { Helmet } from "react-helmet"
 
-class RevealJS extends React.Component {
-  componentDidMount() {
-    Reveal.initialize({
-      width: 900,
-      height: 750,
-      // rtl: true,
-      center: false,
-      slideNumber: true,
-      transition: 'fade',
-      history: true
-    });
-  }
-
-  render() {
-    return (
-      <div className={`site-slides ${this.props.type}-slides`}>
-        <SEO title={this.props.title} />
-        <Helmet>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/github.min.css" />
-        </Helmet>
-        <div className="reveal" id="reveal-top">
-          {this.props.children}
-        </div>
-      </div>
-    )
-  }
-}
-
 const Slides = ({ data }) => {
-  console.log(data);
+  useEffect(() => {
+    if(typeof window !== 'undefined' && window.document) {
+      function revealWrap() {
+        if(window.Reveal == null) {
+          window.setTimeout(revealWrap, 100); /* this checks the flag every 100 milliseconds*/
+        } else {
+          window.Reveal.initialize({
+            width: 900,
+            height: 750,
+            // rtl: true,
+            center: false,
+            slideNumber: true,
+            transition: 'fade',
+            history: true
+          });
+        }
+      }
+      revealWrap();
+    }
+    // const initReveal = require('./init_reveal.js')
+  }, [])
+
   const title = data.slides.headings[0].value;
   const type = data.slides.frontmatter.type;
   return (
-    <RevealJS title={title} type={type}>
-      <div className="slides site-style" dangerouslySetInnerHTML={{ __html: data.slides.html }} />
-    </RevealJS>
+    <div className={`site-slides ${type}-slides`}>
+      <SEO title={title} />
+      <Helmet>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/github.min.css" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/reveal.js/4.1.0/reveal.js"></script>
+        <script src={withPrefix('init_reveal.js')} type="text/javascript"></script>
+      </Helmet>
+      <div className="reveal" id="reveal-top">
+        <div className="slides site-style" dangerouslySetInnerHTML={{ __html: data.slides.html }} />
+      </div>
+    </div>
   )
 }
 
